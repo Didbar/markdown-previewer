c9a79a0a77890cd3ccf00bbda9229ee7058e8ac3
Daviti Didbaridze <ddidbaridze@gmail.com>
2021-03-23 23:02:34 +0100

    Add For FreeCodeCamp

--- src/App.js
+++ src/App.js
@@ -1,25 +1,98 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import marked from "marked";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const placeholder = `# Welcome to my React Markdown Previewer!
+
+## This is a sub-heading...
+### And here's some other cool stuff:
+
+Heres some code, \`<div></div>\`, between 2 backticks.
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+You can also make text **bold**... whoa!
+Or _italic_.
+Or... wait for it... **_both!_**
+And feel free to go crazy ~~crossing stuff out~~.
+
+There's also [links](https://www.freecodecamp.com), and
+> Block Quotes!
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | -------------
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+- And of course there are lists.
+  - Some are bulleted.
+     - With different indentation levels.
+        - That look like this.
+
+
+1. And there are numbererd lists too.
+1. Use just 1s if you want!
+1. And last but not least, let's not forget embedded images:
+
+![React Logo w/ Text](https://create-react-app.dev/img/logo.svg)
+`;
+class App extends Component {
+  state = {
+    text: placeholder,
+  };
+
+  handleChange = (e) => {
+    this.setState({
+      text: e.target.value,
+    });
+  };
+  getMarkdownText = () => {
+    var rawMarkup = marked(this.state.text, { breaks: true });
+    return { __html: rawMarkup };
+  };
+  render() {
+    const { text } = this.state;
+
+    return (
+      <div className="App container-fluid">
+        <header className="header">
+          <h2 className="text-center">Markdown Previewer App</h2>
+        </header>
+        <main className="main">
+          <div className="row">
+            <div className="col-6">
+              <h4 className="text-center">Editor TextArea</h4>
+              <textarea
+                onChange={this.handleChange}
+                value={text}
+                name="editor"
+                id="editor"
+                className="form-control"
+              />
+            </div>
+            <div className="col">
+              <h4 className="text-center">Preview Markdown Text</h4>
+              <div
+                id="preview"
+                dangerouslySetInnerHTML={this.getMarkdownText()}
+              ></div>
+            </div>
+          </div>
+        </main>
+      </div>
+    );
+  }
 }
 
 export default App;
